e15aa4ea2b8b43aa796a1a5c704481326b257900
BarbieSmachh <hicham.-sebbar@hotmail.fr>
2023-06-20 11:26:14 +0200

    exo fini

---
+++ main.js
@@ -0,0 +1,93 @@
+//Exo 1
+// let exo1 = 'osef'
+// alert(exo1)
+//
+//Exo 2
+// let lastName = 'Doe'
+// let firstName = 'John'
+// let city = 'New-York'
+// alert('Nom : '+lastName+'\n'+'Prénom : '+firstName+'\n'+'Ville : '+city  )
+//
+//Exo 3
+// let prenom
+// prenom = prompt("Veuillez saisir votre prénom :")
+// alert('Bonjour '+prenom)
+//
+//Exo 4
+// let Prénom
+// let Nom
+// let Ville
+// Prénom = prompt("Veuillez saisir votre prénom :")
+// Nom = prompt("Veuillez saisir votre nom :");
+// Ville = prompt("Veuillez saisir votre ville :");
+// alert('Nom : '+Nom+'\n'+'Prénom : '+Prénom+'\n'+'Ville : '+Ville )
+//
+//Exo 5
+// let chiffre1
+// let chiffre2
+// chiffre1 = prompt("Veuillez saisir un premier nombre :")
+// chiffre2 = prompt("Veuillez saisir un deuxième nombre :")
+// let chiffre1entier = Math.floor(chiffre1)
+// alert(chiffre1entier*chiffre2)
+//
+//Exo 6
+// let chiffre1 = 7
+// let chiffre2 = 6
+// let reste = (chiffre1 % chiffre2)
+// console.log(reste)
+//
+//Exo 7
+// let pointure
+// let bornyear
+// let resultat
+// pointure = prompt("Veuillez saisir votre pointure :")
+// bornyear = prompt("Veuillez saisir votre année de naissance :")
+// resultat = ((((pointure * 2 + 5) * 50) - bornyear) + 1766)
+// console.log(resultat)
+
+//Exo 8
+// let age
+// age = prompt("Veuillez saisir votre age :")
+// if (age>17){
+//     console.log('Vous êtes majeur')
+// }else{
+//     console.log('Vous êtes mineur')
+// };
+//
+//Exo 9
+// let table = Array.from({length: 10}, (_, index) => index + 1);
+// console.log(table)
+// let i = 1
+// let nombre
+// nombre = prompt("Veuillez saisir un nombre :")
+// while(i<11){
+//     console.log(nombre*i)
+//     i++
+// };
+//
+//Exo 10
+// let mot = prompt("Veuillez saisir un mot :");
+
+// for (var i = 0; i < mot.length; i++) {
+//   console.log(mot[i]);
+// };
+//
+//Exo11
+// let nombre
+// nombre = prompt("Veuillez saisir un nombre entre 1 et 10 :")
+// if(nombre<5){
+//     console.log('Le nombre saisi est inférieur à 5');
+// }else if((nombre>5)&&(nombre<=10)){
+//     console.log('Le nombre saisi est supérieur à 5');
+// }else if(nombre===5){
+//     console.log('Le nombre saisi est égale à 5');
+// }else{(nombre>10)
+//     console.log('j\'ai demandé un nombre entre 1 et 10 connard')
+// }
+//
+//Exo 12
+// let nombre
+// function carré (nombre){
+//     console.log(nombre**2)
+// }
+// carré(nombre = prompt("Veuillez saisir un nombre :"))
